6a776c4bbf2b7156f8a4e49d3878f91286d6d3c0
Sabbir Hossain <82939905+devlopersabbir@users.noreply.github.com>
2021-09-19 09:26:10 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,5 @@
+setInterval(()=>{
+    let number = 1;
+    const folow = document.getElementsByClassName("sqdOP  L3NKy    _8A5w5")[number].click();
+    const unfollow = document.getElementsByClassName("aOOlW -Cab_")[0].click();
+}, 2000);
